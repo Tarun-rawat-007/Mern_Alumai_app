0528aa158f797fe04820c8a13a462a8168d4b2ef
Tarun-rawat-007 <126488730+Tarun-rawat-007@users.noreply.github.com>
2025-04-06 20:14:50 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react(),],
+  build: {
+    chunkSizeWarningLimit: 1000, // increases the limit from 500kb to 1000kb
+  },
 })
